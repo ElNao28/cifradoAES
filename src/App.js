7aa896d5717d5579f27167d177737702efad9c61
ELNAO <luis28jair5@gmail.com>
2023-10-11 23:27:56 -0600

    nuevo

--- src/App.js
+++ src/App.js
@@ -4,39 +4,33 @@ import CryptoJS from 'crypto-js';
 function App() {
   const [inputText, setInputText] = useState('');
   const [encryptedText, setEncryptedText] = useState('');
-  const [decryptedText, setDecryptedText] = useState('');
 
   const encryptText = () => {
-    const key = 'TuClaveSecreta'; // La clave debe ser segura y confidencial
+    const key = 'clave';
     const encrypted = CryptoJS.AES.encrypt(inputText, key).toString();
     setEncryptedText(encrypted);
   }
 
   const decryptText = () => {
-    const key = 'TuClaveSecreta'; // Debe ser la misma clave utilizada para cifrar
-    const decrypted = CryptoJS.AES.decrypt(encryptedText, key).toString(CryptoJS.enc.Utf8);
-    setDecryptedText(decrypted);
+    const key = 'clave';
+    const encrypted = CryptoJS.AES.decrypt(encryptedText, key).toString(CryptoJS.enc.Utf8);
+    setEncryptedText(encrypted);
   }
 
   return (
-    <div>
-      <h1>Cifrado y Descifrado AES en React</h1>
+    <div class="container mt-5 bg-success py-3 px-3 rounded">
+      <h3>Cifrado y Descifrado AES</h3>
       <input
+      class="form-control mb-3"
         type="text"
         value={inputText}
         onChange={e => setInputText(e.target.value)}
         placeholder="Texto a cifrar"
       />
-      <button onClick={encryptText}>Cifrar</button>
-      <div>
-        <strong>Texto cifrado:</strong>
-        <p>{encryptedText}</p>
-      </div>
-      <button onClick={decryptText}>Descifrar</button>
-      <div>
-        <strong>Texto descifrado:</strong>
-        <p>{decryptedText}</p>
-      </div>
+      <button class="btn btn-primary" onClick={encryptText}>Cifrar</button>
+      <button class="btn btn-warning mx-2" onClick={decryptText}>Descifrar</button>
+
+      <input class="form-control mt-3" disabled placeholder='Resultado' value={encryptedText}></input>
     </div>
   );
 }
